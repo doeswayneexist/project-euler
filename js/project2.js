a1cf6999bf112262d86664100dc4cd0cc45eca05
Wayne Smith <doeswayneexist@gmail.com>
2015-05-19 00:56:28 -0700

    added more initializers in the for loop.
    just as cheesy as adding additional final-expressions. but it does
    reduce it...

--- js/project2.js
+++ js/project2.js
@@ -1,7 +1,4 @@
-var firstNumber, secondNumber, fibSum, totalSum;
-firstNumber = secondNumber = 1;
-fibSum = totalSum = 0;
-for (var i = fibSum; i < 4000000; firstNumber = secondNumber, secondNumber = fibSum, i = fibSum) {
+for (firstNumber = secondNumber = 1, fibSum = totalSum = 0, i = fibSum; i < 4000000; firstNumber = secondNumber, secondNumber = fibSum, i = fibSum) {
     fibSum = firstNumber + secondNumber;
     if (fibSum % 2 == 0 && fibSum < 4000000) totalSum = totalSum + fibSum;}
 alert(totalSum);

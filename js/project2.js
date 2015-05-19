62f218fa1f08281eace0e346f86f27c4a083beb4
Wayne Smith <doeswayneexist@gmail.com>
2015-05-19 01:10:36 -0700

    added even more final expressions to the for loop.
    cheesy...

--- js/project2.js
+++ js/project2.js
@@ -1,4 +1,3 @@
-for (firstNumber = secondNumber = 1, fibSum = totalSum = 0, i = fibSum; i < 4000000; firstNumber = secondNumber, secondNumber = fibSum, i = fibSum) {
-    fibSum = firstNumber + secondNumber;
+for (firstNumber = secondNumber = 1, fibSum = totalSum = 0, i = fibSum; i < 4000000; fibSum = firstNumber + secondNumber, firstNumber = secondNumber, secondNumber = fibSum, i = fibSum) {
     if (fibSum % 2 == 0 && fibSum < 4000000) totalSum = totalSum + fibSum;}
 alert(totalSum);

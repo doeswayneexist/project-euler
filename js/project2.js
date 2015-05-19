2ce9990d656f8e98b3840622db032ed171cc87ef
Wayne Smith <doeswayneexist@gmail.com>
2015-05-19 01:21:32 -0700

    turned the for loop into a single line statement.
    its counts!

--- js/project2.js
+++ js/project2.js
@@ -1,3 +1,2 @@
-for (firstNumber = secondNumber = 1, fibSum = totalSum = 0, i = fibSum; i < 4000000; fibSum = firstNumber + secondNumber, firstNumber = secondNumber, secondNumber = fibSum, i = fibSum) {
-    if (fibSum % 2 == 0 && fibSum < 4000000) totalSum = totalSum + fibSum;}
+for (firstNumber = secondNumber = 1, fibSum = totalSum = 0, i = fibSum; i < 4000000; fibSum = firstNumber + secondNumber, firstNumber = secondNumber, secondNumber = fibSum, i = fibSum) if (fibSum % 2 == 0 && fibSum < 4000000) totalSum = totalSum + fibSum;
 alert(totalSum);

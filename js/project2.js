6555a618073552c09b9eaa935e025a9b65cbb627
Wayne Smith <doeswayneexist@gmail.com>
2015-05-19 00:39:13 -0700

    hard coded number cap

--- js/project2.js
+++ js/project2.js
@@ -1,10 +1,9 @@
-var numberCap, firstNumber, secondNumber, fibSum, totalSum;
-numberCap = 4000000;
+var firstNumber, secondNumber, fibSum, totalSum;
 firstNumber = secondNumber = 1;
 fibSum = totalSum = 0;
-for (var i = fibSum; i < numberCap; i = fibSum) {
+for (var i = fibSum; i < 4000000; i = fibSum) {
     fibSum = firstNumber + secondNumber;
-    if (fibSum % 2 == 0 && fibSum < numberCap) totalSum = totalSum + fibSum;
+    if (fibSum % 2 == 0 && fibSum < 4000000) totalSum = totalSum + fibSum;
     firstNumber = secondNumber;
     secondNumber = fibSum;
 }

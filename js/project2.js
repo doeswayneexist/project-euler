635570b0800058eb4fe2815586cdc6cfa9455350
Wayne Smith <doeswayneexist@gmail.com>
2015-05-19 00:52:19 -0700

    added additional incrementers to the for loop
    this technically does not reduce the number of statements, but in a
    cheap way it does.

--- js/project2.js
+++ js/project2.js
@@ -1,10 +1,7 @@
 var firstNumber, secondNumber, fibSum, totalSum;
 firstNumber = secondNumber = 1;
 fibSum = totalSum = 0;
-for (var i = fibSum; i < 4000000; i = fibSum) {
+for (var i = fibSum; i < 4000000; firstNumber = secondNumber, secondNumber = fibSum, i = fibSum) {
     fibSum = firstNumber + secondNumber;
-    if (fibSum % 2 == 0 && fibSum < 4000000) totalSum = totalSum + fibSum;
-    firstNumber = secondNumber;
-    secondNumber = fibSum;
-}
+    if (fibSum % 2 == 0 && fibSum < 4000000) totalSum = totalSum + fibSum;}
 alert(totalSum);

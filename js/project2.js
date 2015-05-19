9241dd8e25b883f75f20427caac9155c5301c1f0
Wayne Smith <doeswayneexist@gmail.com>
2015-05-19 01:48:57 -0700

    test time of execution
    gonna revert this change eventually.

--- js/project2.js
+++ js/project2.js
@@ -1,2 +1,4 @@
-for(c=4000000,x=y=1,z=t=0,i=z;i<c;z=x+y,x=y,y=z,i=z)if(z%2==0&&z<c)t+=z
-alert(t)
+console.time('test')
+for(x=y=1,z=t=0,i=z;i<4000000;z=x+y,x=y,y=z,i=z)if(z%2==0&&z<4000000)t+=z
+//alert(t)
+console.timeEnd('test')
